5d57acc60d0ef2cad16a668696aed7925a253032
Daniel <galvaodev@gmail.com>
2017-11-10 20:03:38 -0200

    [M1#36] Separating into Components

--- src/app.js
+++ src/app.js
@@ -1,42 +1,18 @@
 'use strict'
 
 import React from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 
 const App = () => (
   <div className='app'>
-    <div className='search'>
-      <input type='search' placeholder='Digite o nome do usuário no GitHub' />
-    </div>
-    <div className='user-info'>
-      <img src='https://avatars3.githubusercontent.com/u/5161645?v=4' />
-      <h2>
-        <a href='https://github.com/danielGalvao'>Daniel Galvao</a>
-      </h2>
-      <ul>
-        <li> - Repositorios: 46 </li>
-        <li> - Seguidores: 32 </li>
-        <li> - Seguindo: 64 </li>
-      </ul>
-
-      <div className='actions'>
-        <button>Ver repositórios</button>
-        <button>Ver favoritos</button>
-      </div>
-
-      <div className='repos'>
-        <h2>Repositorios</h2>
-        <ul>
-          <li><a href=''>Nome do repositorio</a></li>
-        </ul>
-      </div>
-
-      <div className='starred'>
-        <h2>Favoritos</h2>
-        <ul>
-          <li><a href=''>Nome do repositorio</a></li>
-        </ul>
-      </div>
-    </div>
+    <Search />
+    <UserInfo />
+    <Actions />
+    <Repos className='repos' title='Repositórios:' repos={[{ name: 'Nome do repositório', link: '#' }]} />
+    <Repos className='starred' title='Favoritos:' repos={[{ name: 'Nome do repositório', link: '#' }]} />
   </div>
 )
 
---
+++ src/components/user-info.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars3.githubusercontent.com/u/5161645?v=4' />
+    <h1 className='username'>
+      <a href='https://github.com/danielGalvao'>Daniel Galvao</a>
+    </h1>
+    <ul>
+      <li> - Repositórios: 46 </li>
+      <li> - Seguidores: 32 </li>
+      <li> - Seguindo: 64 </li>
+    </ul>
+  </div>
+)
+
+export default UserInfo
